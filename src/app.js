0ad7326c7033ecb00ccb4a51859122c5bb00c9b6
Teja Varshith <learnatyour@gmail.com>
2025-12-11 18:31:01 +0530

    init

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/",(req,res) => {
+    res.send("Hello from the server!");
+});
+
+
+
+app.listen(3000, () => {
+    console.log("Server listening at port 3000");
+});
